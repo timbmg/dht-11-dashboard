bb5e687753e568b4b4e07defb91dd376d2946df9
Tim Baumgärtner <baumgaertner.t@gmail.com>
2024-11-12 18:41:56 +0100

    use altair

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
+import altair as alt
 import pandas as pd
-
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import streamlit as st
 from st_supabase_connection import SupabaseConnection, execute_query
 
@@ -23,33 +21,91 @@ df["created_at"] = pd.to_datetime(df["created_at"])
 st.write(f"Number of Measurements: {len(df)}")
 st.write(f"Latest Measurements (UTC): {df['created_at'].max()}")
 
+base = alt.Chart(df).encode(x=alt.X("created_at:T", title="Time"))
 
-# Data Visualization
-fig, ax1 = plt.subplots(figsize=(6 * 1.3, 6))
+hover = alt.selection_point(
+    fields=["created_at"],
+    nearest=True,
+    on="mouseover",
+    empty="none",
+    clear="mouseout",
+)
 
-# First y-axis for temperature
-ax1.plot(df["created_at"], df["temperature"], color="tab:red", label="Temperature (°C)")
-ax1.set_xlabel("Created At")
-ax1.set_ylabel("Temperature (°C)", color="tab:red")
-ax1.tick_params(axis="y", labelcolor="tab:red")
+# Create a line for temperature
+min_temp_scale = df["temperature"].min() - 2
+max_temp_scale = df["temperature"].max() + 2
+temperature_line = base.mark_line(color="red", interpolate="monotone").encode(
+    y=alt.Y(
+        "temperature:Q",
+        title="Temperature (°C)",
+        axis=alt.Axis(titleColor="red"),
+        scale=alt.Scale(domain=[min_temp_scale, max_temp_scale]),
+    )
+)
 
-# Second y-axis for humidity
-ax2 = ax1.twinx()
-ax2.plot(df["created_at"], df["humidity"], color="tab:blue", label="Humidity (%)")
-ax2.set_ylabel("Humidity (%)", color="tab:blue")
-ax2.tick_params(axis="y", labelcolor="tab:blue")
+# Create a line for humidity with a secondary y-axis
+min_humidity_scale = df["humidity"].min() - 5
+max_humidity_scale = df["humidity"].max() + 5
+humidity_line = base.mark_line(color="blue", interpolate="monotone").encode(
+    y=alt.Y(
+        "humidity:Q",
+        title="Humidity (%)",
+        axis=alt.Axis(titleColor="blue"),
+        scale=alt.Scale(domain=[min_humidity_scale, max_humidity_scale]),
+    ),
+)
 
-ax1.xaxis.set_major_locator(
-    mdates.AutoDateLocator()
-)  # Automatically set locator based on data range
-ax1.xaxis.set_major_formatter(
-    mdates.DateFormatter("%Y-%m-%d %H:%M")
-)  # Show full timestamp
-fig.autofmt_xdate()  # Rotate date labels for readability
 
-# Title and layout
-fig.suptitle("Temperature and Humidity Over Time")
-fig.tight_layout()
+# hover_line = (
+#     base.mark_rule(color="gray", strokeDash=[5, 5])
+#     .encode(opacity=alt.condition(hover, alt.value(1), alt.value(0)))
+#     .add_params(hover)
+# )
 
-st.pyplot(fig)
-# plt.show()
+# Points to show nearest values on hover for temperature and humidity
+# temperature_points = (
+#     base.mark_circle(color="red", size=50)
+#     .encode(
+#         y="temperature:Q",
+#         opacity=alt.condition(hover, alt.value(1), alt.value(0)),
+#     )
+#     .transform_filter(hover)
+# )
+
+# humidity_points = (
+#     base.mark_circle(color="blue")
+#     .encode(y="humidity:Q", opacity=alt.condition(hover, alt.value(1), alt.value(0)))
+#     .transform_filter(hover)
+# )
+
+# Tooltip to display temperature and humidity values on hover
+tooltips = (
+    base.mark_rule()
+    .encode(
+        opacity=alt.condition(hover, alt.value(0.1), alt.value(0)),
+        tooltip=[
+            # alt.Tooltip("created_at:T", title="Time"), 
+            # show the full timestamp in the tooltip
+            alt.Tooltip("created_at:T", title="Time", format="%Y-%m-%d %H:%M"),
+            alt.Tooltip("temperature:Q", title="Temperature (°C)"),
+            alt.Tooltip("humidity:Q", title="Humidity (%)"),
+        ],
+    )
+    .add_params(hover)
+)
+
+# Layer all elements
+chart = (
+    alt.layer(
+        temperature_line,
+        humidity_line,
+        # hover_line,
+        # temperature_points,
+        # humidity_points,
+        tooltips,
+    )
+    .resolve_scale(y="independent")
+    .properties(width=600, height=400, title="Temperature and Humidity over Time")
+)
+
+st.altair_chart(chart, use_container_width=True)
