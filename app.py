fbf410b6bb20b84432ce899d9a2ea87287bf5a58
Tim Baumgärtner <baumgaertner.t@gmail.com>
2024-11-15 11:51:05 +0100

    show only last 24 hours

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ st_supabase_client = st.connection(
     ttl=None,
 )
 
+# get the latets 1440 measurements, which is 24 hours of data
 rows = execute_query(
-    st_supabase_client.table("dht11").select("*").eq("location", "bedroom"),
+    st_supabase_client.table("dht11")
+    .select("*")
+    .eq("location", "bedroom")
+    .order("created_at", ascending=False)
+    .limit(1440),
     ttl="10m",
 )
 
